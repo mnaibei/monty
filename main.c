957d42719e2628fff0dc7553a53a2ff8ddd699eb
Mucha <muchajulius@gmail.com>
2021-09-25 10:47:48 +0300

    .

--- main.c
+++ main.c
@@ -1,52 +1,36 @@
-#ifndef MONTY_H
-#define MONTY_H
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include "monty.h"
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * main - monty interpreter
+ * @ac: number of arguments used in command call
+ * @av: array of arguments in command call
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Return: appropriate error
  */
-typedef struct stack_s
-{
-int n;
-struct stack_s *prev;
-struct stack_s *next;
-} stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+/*
+ * TODO
+ *in push if int is not an int or if no argument given to push
+ *	print the error message L<line_number>: usage: push integer,
+ *	followed by a new line, and exit with the status EXIT_FAILURE
+ * exit correctly (free stuff)
+ * malloc failed checks: Error: malloc failed, exit failure
  *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct instruction_s
+int main(int ac, char **av)
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-extern char **curIns;
-void getLinez(char *fileName);
-void setGlobalVarArray(char *line, int lineNum, stack_t **head, FILE *file);
-void rnOp(instruction_t fL[], int lNum, stack_t **head, char *buf, FILE *file);
-void exitFunc(stack_t **stack, char *buf, FILE *file);
-int checkForChars(char *str);
-void pushOp(stack_t **stack, unsigned int line_number);
-void pallOp(stack_t **stack, unsigned int line_number);
-void pintOp(stack_t **stack, unsigned int line_number);
-void popOp(stack_t **stack, unsigned int line_number);
-void swapOp(stack_t **stack, unsigned int line_number);
-void addOp(stack_t **stack, unsigned int line_number);
-void nopOp(stack_t **stack, unsigned int line_number);
-void freeList(stack_t *head);
-#endif
+if (ac != 2)
+{
+dprintf(STDERR_FILENO, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+if (access(av[1], F_OK) != 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
+exit(EXIT_FAILURE);
+}
+else
+{
+getLinez(av[1]);
+return (EXIT_SUCCESS);
+}
+}
